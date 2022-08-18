95bf689a63b3309345c097686867f606d74a1fde
Alexandre Morais <alexandre@a2o.com.br>
2022-08-18 13:37:24 -0300

    Ajustado tela home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>React Native</Text>
-      <Text style={styles.eventName}>React</Text>
-      <Text style={styles.eventName}>Alexandre Fereira Morais</Text>
+      <Text style={styles.eventName}>
+        Nome do Evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+      
+      <Participant></Participant>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,12 +2,46 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'black', 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
+    backgroundColor: '#131016', 
+    flex: 1,
+    padding: 24
   },
   eventName: {
-    color: 'red'
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    width: '100%',
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: 'white',
+    padding: 16,
+    marginRight: 7
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42
   }
 });
