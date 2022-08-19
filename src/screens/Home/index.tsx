84bec910d1961f33e6b8e7f5149172847322ca7c
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2022-08-18 23:11:39 -0300

    Trabalhando com alertas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
@@ -6,10 +6,23 @@ export function Home() {
   const participants = ['Alexandre', 'Jonas', 'Rodrigo', 'Diego', 'Maria', 'Joana', 'Jorge', 'Feliz', 'João'];
 
   function handleParticipantAdd() {
+    if (participants.includes('Alexandre')) {
+      return Alert.alert('Erro', 'Participante já existe');
+    }
     console.log('Adicionou participante');    
   }
   
   function handleParticipantRemove(name: string) {
+    Alert.alert('Atenção', `Deseja Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Sucesso!', 'Deletado.')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
     console.log(`Removeu participante ${name}`);
   }
 
